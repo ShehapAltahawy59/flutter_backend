e5946066c94cd374c5b5c015f50ae52f25241a38
shehap62 <shehapkhalil62@gmail.com>
2025-06-19 00:16:09 +0300

    edit

--- routes/emergency_routes.py
+++ routes/emergency_routes.py
@@ -6,8 +6,23 @@ from bson import json_util, ObjectId
 import json
 import firebase_admin
 from firebase_admin import credentials, messaging
+import os
+firebase_creds = {
+            "type": os.getenv("FIREBASE_TYPE"),
+            "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+            "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+            "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
+            "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+            "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+            "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
+            "token_uri": os.getenv("FIREBASE_TOKEN_URI"),
+            "auth_provider_x509_cert_url": os.getenv("FIREBASE_AUTH_PROVIDER_CERT_URL"),
+            "client_x509_cert_url": os.getenv("FIREBASE_CLIENT_CERT_URL")
+        }
 
-cred = credentials.Certificate("firebase-adminsdk.json")
+        # Initialize Firebase Admin SDK with the constructed credentials
+cred = credentials.Certificate(firebase_creds)
+
 firebase_admin.initialize_app(cred)
 
 emergency_bp = Blueprint('emergency', __name__, url_prefix='/api/emergency')
